b02041549e4236b522a8cc3c03f85ff6d981a77b
vagrant <alvin.cl.ng@gmail.com>
2017-04-28 07:08:59 +0000

    Made CSS for front of card, Discard 3 and Discard 4

--- server.js
+++ server.js
@@ -43,6 +43,10 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/game", (req, res) => {
+  res.render("game");
+});
+
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });

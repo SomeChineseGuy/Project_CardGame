e9fa60fb60eca3262185d2a2e986e5135e248dac
Charles <cming.chou@gmail.com>
2017-04-29 02:23:53 +0000

    sockets passing altered  private objects upon  button event to respective user/opponent playerss

--- server.js
+++ server.js
@@ -59,12 +59,20 @@ app.get('/login/:id', (req, res) => {
  res.redirect('/');
 });
 
-app.get('/game', function(req, res,next) {
+app.get('/logout', (req, res) => {
+    req.session = null;
+    res.redirect('/');
+  });
 
-   res.render("game1");
+app.get('/game', function(req, res) {
+  if(players.host.id && players.guest.id){
+      res.redirect('/');
+      return;
+    };
+  res.render("game1");
 });
 
-app.get('/game2', function(req, res,next) {
+app.get('/game2', function(req, res) {
    res.sendFile(__dirname + '/public/game2.html');
 });
 
@@ -73,77 +81,150 @@ server.listen(PORT, () => {
 });
 
 const user1 = {
-  deck: true,
-  discard: 1,
-  userhand: [2,3,4,5,6],
+  deck: 30,
+  discard: [1,0,1],
+  userhand: [[1,1,2],[1,2,3],[1,3,4],[2,0,5],[2,1,6]],
   oppHandCount: 4,
-  userDiscard: [52,51,50],
-  opponentDiscard: [48,47,46]
+  dropPile: [[13,4,52],[13,3,51],[13,2,50]],
+  availablePlays: {
+    draw: 0,
+    takeTop: 0,
+    takeAll: 0,
+    drop3: 1,
+    drop1: 1,
+    discard: 1
+  }
 }
 
 const user2 = {
-  deck: true,
-  discard: 1,
-  userhand: [7,8,9,10],
+  deck: 30,
+  discard: [1,0,1],
+  userhand: [[2,2,7],[2,3,8],[3,0,9],[3,2,10]],
   oppHandCount: 5,
-  userDiscard: [48, 47, 46],
-  opponentDiscard: [52, 51, 50]
+  dropPile: [[13,4,52],[13,3,51],[13,2,50]],
+  availablePlays:{
+    draw: 0,
+    takeTop: 0,
+    takeAll: 0,
+    drop3: 0,
+    drop1: 0,
+    discard: 0
+  }
 }
 
-const authUsers = {
+const user = {
+  deck: 40,
+  discard: [1,0,1],
+  userhand: [[1,1,2],[1,2,3],[1,3,4],[2,0,5],[2,1,6]],
+  oppHandCount: 4,
+  dropPile: [[13,4,52],[13,3,51],[13,2,50]],
+  availablePlays: {
+    draw: 0,
+    takeTop: 0,
+    takeAll: 0,
+    drop3: 1,
+    drop1: 1,
+    discard: 1
+  }
+}
 
+const opp = {
+  deck: 50,
+  discard: [1,0,1],
+  userhand: [[2,2,7],[2,3,8],[3,0,9],[3,2,10]],
+  oppHandCount: 5,
+  dropPile: [[13,4,52],[13,3,51],[13,2,50]],
+  availablePlays:{
+    draw: 0,
+    takeTop: 0,
+    takeAll: 0,
+    drop3: 0,
+    drop1: 0,
+    discard: 0
+  }
 }
 
 
-let connectedPlayers = {
+
+function userID(socketid) { return players.host.socket.id === socketid ? players.host.id : players.guest.id; }
+function oppID (socketid) { return players.host.socket.id === socketid ? players.guest.id : players.host.id; }
+function userSocket (socketid) { return players.host.socket.id === socketid ? players.host.socket : players.guest.socket;}
+function oppSocket (socketid) { return players.host.socket.id === socketid ? players.guest.socket : players.host.socket;}
+function resetPlayer(obj) { obj.id = null; obj.socket = null};
+function userState(socketid) {return players.host.socket.id === socketid ? players.host.state : players.guest.state;}
+function oppState (socketid) { return players.host.socket.id === socketid ? players.guest.state : players.host.state; }
+function userHostGuest (socketid) {return players.host.socket.id === socketid ? 'host' : 'guest'}
+function oppHostGuest (socketid) {return players.host.socket.id === socketid ? 'guest' : 'host'}
+
+
+let players = {
   host: {
     id: null,
-    socket: null
+    socket: null,
+    state: user1
   },
   guest: {
     id: null,
-    socket: null
+    socket: null,
+    state: user2
   }
 };
-
-// var sessionMiddleware = cookieSession({
-//   name: 'session',
-//   keys: ['Lighthouse'],
-//   maxAge: 24 * 60 * 60 * 1000
-// });
 
 io.use(function(socket, next) {
     sessionMiddleware(socket.request, socket.request.res, next);
 });
 
-const game1 = io.of('/game1');
-game1.on('connection', function(socket) {
-  socket.on('add user', function(username) {
+console.log('initial players------------------------', players);
 
-    if (connectedPlayers.host.socket === null) {
-      connectedPlayers.host.socket = socket.id;
-      connectedPlayers.host.id = socket.request.session.userid;
-    } else {
-      connectedPlayers.guest.socket = socket.id;
-      connectedPlayers.guest.id = socket.request.session.userid;
+const game1 = io.of('/game1');
+
+game1.on('connection', function(socket) {
+  socket.on('add user', function(socketid) {
+    if (players.host.socket === null) {
+      players.host.socket = socket;
+      players.host.id = socket.request.session.userid;
+      console.log('Host Joins --------------', players);
+    } else if (players.host.socket && players.guest.socket === null) {
+      players.guest.socket = socket;
+      players.guest.id = socket.request.session.userid;
+      game1.emit('game ready');
+      players.host.socket.emit('start state', players.host.state);
+      players.guest.socket.emit('start state', players.guest.state);
+      console.log('GUEST JOINS ===============', players);
     }
+
     const connections = io.of('/game1').connected;
 
     if (connections) {
-      if (connectedPlayers.host.socket) {
-        const host = connections[connectedPlayers.host.socket];
-        if (host) {
-          host.emit('game object', `you are the host with userid ${connectedPlayers.host.id} and ${connectedPlayers.host.socket}`);
-        }
+      if (players.host.socket) {
+          players.host.socket.emit('test1', `you are the host with userid ${players.host.id} and ${players.host.socket.id}`);
+
       }
 
-      if (connectedPlayers.guest.socket) {
-        connections[connectedPlayers.guest.socket].emit('game object', `you are the guest with userid ${connectedPlayers.guest.id} and ${connectedPlayers.guest.socket}`);
+      if (players.guest.socket) {
+        players.guest.socket.emit('test2', `you are the guest with userid ${players.guest.id} and ${players.guest.socket.id}`);
 
       }
     }
 
   });
+  socket.on('disconnect', () =>{
+      if(socket.id === players.host.socket.id){
+        resetPlayer(players.host);
+        console.log(players);
+      }
+      else{
+        resetPlayer(players.guest);
+        console.log(players);
+      }
+    })
+  socket.on('draw', (socketid)=> {
+    console.log(socketid);
+    players[userHostGuest(socketid)].state = user;
+    players[oppHostGuest(socketid)].state = opp;
+    userSocket(socketid).emit('game object', userState(socketid));
+    oppSocket(socketid).emit('game object', oppState(socketid));
+  })
 });
 
 const game2 = io.of('/game2');

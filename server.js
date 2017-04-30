cf7449a63a16b48a4a1fbfdc45c27a09c1a80e56
Edurne Berastegi Zabalza <edurne.berastegi@gmail.com>
2017-04-30 06:52:30 +0000

    clean server from non used routes

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
-"use strict";
+'use strict';
 
 require('dotenv').config();
 
 
 const PORT        = process.env.PORT || 8080;
-const ENV         = process.env.NODE_ENV || "development";
-const express     = require("express");
-const bodyParser  = require("body-parser");
-const sass        = require("node-sass-middleware");
+const ENV         = process.env.NODE_ENV || 'development';
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const sass        = require('node-sass-middleware');
 const app         = express();
 const server      = require('http').createServer(app);
-const knexConfig  = require("./knexfile");
-const knex        = require("knex")(knexConfig[ENV]);
-const morgan      = require('morgan');
-const knexLogger  = require('knex-logger');
+const knexConfig  = require('./knexfile');
+const knex        = require('knex')(knexConfig[ENV]);
 const cookieSession = require('cookie-session');
 const io          = require('socket.io')(server);
 const deckConstructor = require ('./games/deck.js');
 const rummy = require('./games/rummy.js');
-// Seperated Routes for each Resource
-const usersRoutes = require("./routes/users");
+
+let morgan;
+let knexLogger;
+if(process.env.NODE_ENV === 'development'){
+  morgan      = require('morgan');
+  knexLogger  = require('knex-logger');
+}
+
+
+const usersRoutes = require('./routes/users');
 
 const sessionMiddleware = cookieSession({
   name: 'session',
@@ -29,52 +35,34 @@ const sessionMiddleware = cookieSession({
 
 app.use(sessionMiddleware);
 
-// Load the logger first so all (static) HTTP requests are logged to STDOUT
-// 'dev' = Concise output colored by response status for development use.
-//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
-app.use(morgan('dev'));
+if(process.env.NODE_ENV === 'development'){
+  app.use(morgan('dev'));
+  app.use(knexLogger(knex));
+}
 
-// Log knex SQL queries to STDOUT as well
-app.use(knexLogger(knex));
-
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/styles", sass({
-  src: __dirname + "/styles",
-  dest: __dirname + "/public",
+app.use('/styles', sass({
+  src: __dirname + '/styles',
+  dest: __dirname + '/public',
   outputStyle: 'expanded',
   prefix: '/styles',
   force: true
 }));
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // Mount all resource routes
-app.use("/api/users", usersRoutes(knex));
+app.use('/api/users', usersRoutes(knex));
 
 // Home page
-app.get("/", (req, res) => {
-  res.render("index");
+app.get('/', (req, res) => {
+  let username = null;
+  if(req.session.userid){
+    username = 'nyan cat';
+  }
+  res.render('index', {username});
 });
 
-// TEST FOR TEMPLATE!!!!!!!!!!!!!!!!!!
-app.get('/json', (req, res) => {
-  const user2 = {
-    deck: 30,
-    discard: [1, 0, 1],
-    userhand: [[0, 0, 7]],
-    oppHandCount: 5,
-    dropPile: [[13, 4, 52], [13, 3, 51], [13, 2, 50]],
-    availablePlays: {
-      draw: 0,
-      takeTop: 0,
-      takeAll: 0,
-      drop3: 0,
-      drop1: 0,
-      discard: 0
-    }
-  };
-  res.json(user2);
-});
 
 app.get('/login/:id', (req, res) => {
   req.session.userid = req.params.id;
@@ -86,104 +74,29 @@ app.get('/logout', (req, res) => {
     res.redirect('/');
   });
 
-
-
-server.listen(PORT, () => {
-  console.log("Example app listening on port " + PORT);
-});
-
-const user1 = {
-  deck: 30,
-  discard: [1,0,1],
-  userhand: [[1,1,2],[1,2,3],[1,3,4],[2,0,5],[2,1,6]],
-  oppHandCount: 4,
-  dropPile: [[13,4,52],[13,3,51],[13,2,50]],
-  availablePlays: {
-    draw: 0,
-    takeTop: 0,
-    takeAll: 0,
-    drop3: 1,
-    drop1: 1,
-    discard: 1
-  }
-}
-
-
-const user2 = {
-  deck: 30,
-  discard: [1, 0, 1],
-  userhand: [[0, 0, 7], [1, 0, 8], [2, 0, 9], [3, 0, 10], [4, 0, 8], [5, 0, 9], [6, 0, 10], [7, 0, 8], [8, 0, 9], [9, 0, 10], [10, 0, 8], [11, 0, 9], [12, 0, 10],
-               [0, 1, 7], [1, 1, 8], [2, 1, 9], [3, 1, 11], [4, 1, 8], [5, 1, 9], [6, 1, 11], [7, 1, 8], [8, 1, 9], [9, 1, 11], [10, 1, 8], [11, 1, 9], [12, 1, 10],
-               [0, 2, 7], [1, 2, 8], [2, 2, 9], [3, 2, 12], [4, 2, 8], [5, 2, 9], [6, 2, 12], [7, 2, 8], [8, 2, 9], [9, 2, 12], [10, 2, 8], [11, 2, 9], [12, 2, 10],
-               [0, 3, 7], [1, 3, 8], [2, 3, 9], [3, 3, 11], [4, 3, 8], [5, 3, 9], [6, 3, 11], [7, 3, 8], [8, 3, 9], [9, 3, 11], [10, 3, 8], [11, 3, 9], [12, 3, 10]],
-  oppHandCount: 5,
-  dropPile: [[13, 4, 52], [13, 3, 51], [13, 2, 50]],
-  availablePlays: {
-    draw: 0,
-    takeTop: 0,
-    takeAll: 0,
-    drop3: 0,
-    drop1: 0,
-    discard: 0
-  }
-}
-
-
 app.get('/game', function(req, res) {
   if(players.host.id && players.guest.id){
     res.redirect('/');
     return;
   }
-  res.render("game", user2);
+  res.render('game');
 });
 
 app.get('/about', function(req, res, next) {
-  res.render("about");
+  res.render('about');
 });
 
 app.get('/about/shuffle1', function(req, res, next) {
-  res.render("shuffle1");
+  res.render('shuffle1');
 });
 
 app.get('/about/shuffle2', function(req, res, next) {
-  res.render("shuffle2");
+  res.render('shuffle2');
 });
 
-const user = {
-  deck: 40,
-  discard: [1,0,1],
-  userhand: [[1,1,2],[1,2,3],[1,3,4],[2,0,5],[2,1,6]],
-  oppHandCount: 4,
-  dropPile: [[13,4,52],[13,3,51],[13,2,50]],
-  availablePlays: {
-    draw: 0,
-    takeTop: 0,
-    takeAll: 0,
-    drop3: 1,
-    drop1: 1,
-    discard: 1
-  }
-}
-
-const opp = {
-  deck: 50,
-  discard: [1,0,1],
-  userhand: [[2,2,7],[2,3,8],[3,0,9],[3,2,10]],
-  oppHandCount: 5,
-  dropPile: [[13,4,52],[13,3,51],[13,2,50]],
-  availablePlays:{
-    draw: 0,
-    takeTop: 0,
-    takeAll: 0,
-    drop3: 0,
-    drop1: 0,
-    discard: 0
-  }
-}
-
-
-
-
+server.listen(PORT, () => {
+  console.log('Example app listening on port ' + PORT);
+});
 
 function userID(socketid) { return players.host.socket.id === socketid ? players.host.id : players.guest.id; }
 function oppID (socketid) { return players.host.socket.id === socketid ? players.guest.id : players.host.id; }
@@ -196,9 +109,7 @@ function userHostGuest (socketid) {return players.host.socket.id === socketid ? 
 function oppHostGuest (socketid) {return players.host.socket.id === socketid ? 'guest' : 'host'}
 
 
-
 const startGameState = rummy.startGame();
-console.log(startGameState);
 let prevGameState, gameState;
 
 const players = {
@@ -215,8 +126,6 @@ const players = {
 io.use(function(socket, next) {
   sessionMiddleware(socket.request, socket.request.res, next);
 });
-
-console.log('initial players------------------------', players);
 
 const game = io.of('/game');
 
@@ -259,7 +168,6 @@ game.on('connection', function(socket) {
     }
   });
   socket.on('draw', (socketid)=> {
-
     const playerId = userID(socketid);
     const opponentId = oppID(socketid);
     gameState = rummy.drawCard(gameState, playerId, false);
@@ -307,6 +215,4 @@ game.on('connection', function(socket) {
       oppSocket(socketid).emit('turnStart');
     }
   })
-
-
 });

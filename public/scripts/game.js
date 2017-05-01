8c793a283cb48406183888794ba35eafc44c2e93
vagrant <alvin.cl.ng@gmail.com>
2017-05-01 16:55:15 +0000

    Testing func

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -17,13 +17,6 @@
     }
 
 
-  function endTurn() { /*$('.play').prop('disabled', true)*/}
-  function endStart() { /*$('.play.start').prop('disabled', true)*/}
-  function startTurn() {/*$('.play').prop('disabled', false)*/}
-
-    // function endTurn() { $('.play').prop('disabled', true); }
-    // function endStart() { /*$('.play.start').prop('disabled', true)*/ }
-    // function startTurn() { $('.play').prop('disabled', false); }
 
 
 
@@ -46,20 +39,20 @@
     // });
     socket.on('start game', (stateData) => {
       renderPage(stateData);
-      console.log(stateData);
+      
     });
     socket.on('firstTurn', () => endStart());
     socket.on('waitTurn', () => {
-      endTurn();
+
       $('body').append("<p class='waiting'>Waiting for Opponent</p>");
     });
     socket.on('startTurn', () => {
-      startTurn();
+
       $('p.waiting').remove();
     });
     socket.on('new state', (stateData) => {
       renderPage(stateData);
-      console.log(stateData);
+    
 
     });
 
@@ -76,18 +69,18 @@
 
     $(document).on('click', '.draw', (e) => {
       console.log('click');
-      endStart();
+    
       e.preventDefault();
       socket.emit('draw', '/game#' + socket.id);
     });
 
     $(document).on('click', '.takeTop', (e) => {
-      endStart();
+    
       e.preventDefault();
       socket.emit('takeTop', '/game#' + socket.id);
     });
     $(document).on('click', '.takeAll', (e) => {
-      endStart();
+    
       e.preventDefault();
       socket.emit('takeAll', '/game#' + socket.id);
     });
@@ -105,59 +98,5 @@
       e.preventDefault();
       socket.emit('attachOne', '/game#' + socket.id);
     });
-
-  socket.on('startTurn', () => {
-    startTurn();
-    $('p.waiting').remove();
-  })
-  socket.on('new state', (stateData) => {
-    renderPage(stateData);
-    console.log(stateData);
-
   });
-
-  socket.on('winner', () => {
-    $('body').append("<div class='win'>Congratulations you win! </div>")
-    setTimeout(()=> window.location = '/', 5000);
-  });
-
-  socket.on('loser', () => {
-    $('body').append("<div class='lose'>Sorry you lost</div>");
-    setTimeout(() => window.location = '/', 5000);
-  });
-
-
-  $(document).on('click','.draw', (e) => {
-    console.log('click');
-    endStart();
-    e.preventDefault();
-    socket.emit('draw', '/game#' + socket.id);
-  });
-
-  $(document).on('click', '.takeTop', (e) => {
-    endStart();
-    e.preventDefault();
-    socket.emit('takeTop', '/game#' + socket.id);
-  });
-  $(document).on('click', '.takeAll', (e) => {
-    endStart();
-    e.preventDefault();
-    socket.emit('takeAll', '/game#' + socket.id);
-  });
-
-  $(document).on('click','.discard', (e) => {
-    e.preventDefault();
-    socket.emit('discard', '/game#' + socket.id);
-  });
-
-  $(document).on('click', '.dropSet', (e) => {
-    e.preventDefault();
-    socket.emit('dropSet', '/game#' + socket.id);
-  });
-  $(document).on('click', '.attachOne', (e) => {
-    e.preventDefault();
-    socket.emit('attachOne', '/game#' + socket.id)
-  });
-});
-
 

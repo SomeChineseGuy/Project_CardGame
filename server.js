e44deb6f9126f2a321bafc5f57ac674142ce8f06
Edurne Berastegi Zabalza <edurne.berastegi@gmail.com>
2017-05-01 20:57:35 +0000

    fixed the start game status

--- server.js
+++ server.js
@@ -82,28 +82,6 @@ app.get('/', (req, res) => {
 });
 
 
-
-// TEST FOR TEMPLATE!!!!!!!!!!!!!!!!!!
-app.get('/json', (req, res) => {
-  const user2 = {
-    deck: 30,
-    discard: [1, 0, 1],
-    userhand: [[1, 3, 7], [1, 3, 8], [2, 3, 9], [3, 3, 10], [4, 3, 8], [5, 3, 9], [6, 3, 10], [7, 3, 8], [8, 3, 9], [9, 3, 10], [10, 0, 8], [11, 0, 9], [12, 0, 10],
-               [0, 1, 7], [1, 1, 8], [2, 1, 9], [3, 1, 11], [4, 1, 8], [5, 1, 9], [6, 1, 11], [7, 1, 8], [8, 1, 9], [9, 1, 11], [10, 1, 8], [11, 1, 9]],
-    oppHandCount: 5,
-    dropPile: [[13, 4, 52], [13, 3, 51], [13, 2, 50]],
-    availablePlays: {
-      draw: 0,
-      takeTop: 0,
-      takeAll: 0,
-      drop3: 0,
-      drop1: 0,
-      discard: 0
-    }
-  };
-  res.json(user2);
-});
-
 // knex.select(knex.raw('COUNT(*) AS games, SUM(CASE WHEN matches.winner_id = ? THEN 1 ELSE O END) AS wins', [user_id]))
 // .from('sessions').leftJoin('matches', 'sessions.match_id', 'matches.id')
 // .where('sessions.user_id', user_id).then((row) => {
@@ -239,8 +217,7 @@ function insertWinner (match, winner){
 }
 
 
-const startGameState = rummy.startGame();
-let prevGameState, gameState;
+let gameState;
 
 const players = {
   host: {
@@ -273,8 +250,8 @@ game.on('connection', function(socket) {
       insertMatchInfo();
       game.emit('game ready');
       const deck = deckConstructor.getDeck();
-      const startGameState = rummy.startGame(deck, players.host.id, players.guest.id);
-      gameState = rummy.drawCard(startGameState,players.host.id, true);
+      gameState = rummy.startGame(deck, players.host.id, players.guest.id);
+      gameState = rummy.drawCard(gameState,players.host.id, true);
       const hostView = rummy.filterGameStateForUser(gameState, players.host.id);
       hostView.moves = rummy.getMoves(gameState, players.host.id, false);
       const guestView = rummy.filterGameStateForUser(gameState, players.guest.id);
